939feaa7df029f3a602af8f1d5736fe76681e99b
Yashwanth Remidi <yashwanthremidi@gmail.com>
2023-08-18 23:48:12 +0530

    feat: add movie-tickets-system code

---
+++ low-level-design/movie-tickets/booking.py
@@ -0,0 +1,14 @@
+class Booking: 
+  # Data members
+  def __init__(self, booking_id, amount, total_seats, created_on, status, payment, show):
+    self.__booking_id = booking_id
+    self.__amount = amount
+    self.__total_seats = total_seats
+    self.__created_on = created_on
+    self.__status = status # BookingStatus enum
+    
+    # Instances of classes
+    self.__payment = payment
+    self.__show = show
+    self.__tickets = [] # List of movie tickets
+    self.__seats = [] # List of seats
